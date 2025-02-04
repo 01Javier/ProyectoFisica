56702e9807130583da913cd38595b20428444525
Javili <jjjavier557@gmail.com>
2025-02-04 01:48:30 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/programa/Programa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa;
+
+/**
+ *
+ * @author jjjav
+ */
+public class Programa {
+
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        m.setVisible(true);
+    }
+}
